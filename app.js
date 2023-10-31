fa75d4d68cb009975a3dd57f13252463fd9106af
Omiso <misasuzuki1027@gmail.com>
2023-10-31 18:45:41 +0900

    減った割合

--- app.js
+++ app.js
@@ -33,11 +33,11 @@ rl.on('close', () => {
   for (const [key, value] of prefectureDataMap) {
     value.change = value.after / value.before;
   }
-  const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-    return pair2[1].change - pair1[1].change;
+  const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2)=> {
+    return pair1[1].change - pair2[1].change;
   });
-  const rankingStrings = rankingArray.map(([key, value]) => {
-    return `${key}: ${value.before}=>${value.after} 変化率: ${value.change}`;
+  const rankingStrings = rankingArray.map(([key, value],i) => {
+    return `${i + 1}位 ${key}: ${value.before}=>${value.after} 変化率: ${value.change}`;
   });
   console.log(rankingStrings);
 });
